495278d3235b90933cc2f866fad1adffe90e128d
makekop <72009584+makekop@users.noreply.github.com>
2025-05-08 17:09:18 +0300

    removed unecessary code for user input

--- jobot.py
+++ jobot.py
@@ -15,20 +15,17 @@ def main():
     header_printed = False
     found = False
 
-    if question == "job" :
-        for link in doc.find_all("a", href=True):
-            text = link.get_text(strip=True)
-            href = link["href"].replace("?duplicate=true", "").strip()
+    for link in doc.find_all("a", href=True):
+        text = link.get_text(strip=True)
+        href = link["href"].replace("?duplicate=true", "").strip()
 
-            if ("/open-positions/" in href.lower()):
-                cleaned_text = text.replace("Helsinki", "").replace("Tech", "").replace("Data & Analytics", "").replace("New GamesMarketing", "").replace("Merge MansionProduct Management & Berlin", "").replace(",", "").strip()
-                if not header_printed:
-                    print(f"Here are our current job openings: " )
-                    header_printed = True
+        if ("/open-positions/" in href.lower()):
+            cleaned_text = text.replace("Helsinki", "").replace("Tech", "").replace("Data & Analytics", "").replace("New GamesMarketing", "").replace("Merge MansionProduct Management & Berlin", "").replace(",", "").strip()
+            if not header_printed:
+                print(f"Here are the current job openings: " )
+                header_printed = True
             
-                print(f"{cleaned_text}: www.metacoregames.com{href}")
-                found = True
-    else:
-        print("Currently I can only help with job openings")
+            print(f"{cleaned_text}: www.metacoregames.com{href}")
+            found = True
 if __name__ == "__main__":
     main()
