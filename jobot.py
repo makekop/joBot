8b7a59755d3ad5c5c2747697c91ac329fccb3393
makekop <72009584+makekop@users.noreply.github.com>
2025-05-08 15:41:09 +0300

    first commit on job search mvp

---
+++ jobot.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+def main():
+    from bs4 import BeautifulSoup
+    import requests
+
+    url = "https://metacoregames.com/careers"
+
+    result = requests.get(url)
+    doc = BeautifulSoup(result.text, "html.parser")
+
+    question = "job"
+
+
+    header_printed = False
+    found = False
+
+    if question == "job" :
+        for link in doc.find_all("a", href=True):
+            text = link.get_text(strip=True)
+            href = link["href"].replace("?duplicate=true", "").strip()
+
+            if ("/open-positions/" in href.lower()):
+                cleaned_text = text.replace("Helsinki", "").replace("Tech", "").replace("Data & Analytics", "").replace("New GamesMarketing", "").replace("Merge MansionProduct Management & Berlin", "").replace(",", "").strip()
+                if not header_printed:
+                    print(f"Here are our current job openings: " )
+                    header_printed = True
+            
+                print(f"{cleaned_text}: www.metacoregames.com{href}")
+                found = True
+    else:
+        print("Currently I can only help with job openings")
+if __name__ == "__main__":
+    main()
