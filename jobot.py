9fcd946995a58342f0dd5271dc6d70d24ff9e77f
makekop <72009584+makekop@users.noreply.github.com>
2025-05-08 17:20:13 +0300

    formatted the code properly and removed unused variables

--- jobot.py
+++ jobot.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+
 def main():
     from bs4 import BeautifulSoup
     import requests
@@ -9,23 +10,28 @@ def main():
     result = requests.get(url)
     doc = BeautifulSoup(result.text, "html.parser")
 
-    question = "job"
-
-
     header_printed = False
-    found = False
 
     for link in doc.find_all("a", href=True):
         text = link.get_text(strip=True)
         href = link["href"].replace("?duplicate=true", "").strip()
 
-        if ("/open-positions/" in href.lower()):
-            cleaned_text = text.replace("Helsinki", "").replace("Tech", "").replace("Data & Analytics", "").replace("New GamesMarketing", "").replace("Merge MansionProduct Management & Berlin", "").replace(",", "").strip()
+        if "/open-positions/" in href.lower():
+            cleaned_text = (
+                text.replace("Helsinki", "")
+                .replace("Tech", "")
+                .replace("Data & Analytics", "")
+                .replace("New GamesMarketing", "")
+                .replace("Merge MansionProduct Management & Berlin", "")
+                .replace(",", "")
+                .strip()
+            )
             if not header_printed:
-                print(f"Here are the current job openings: " )
+                print(f"Here are the current job openings: ")
                 header_printed = True
-            
+
             print(f"{cleaned_text}: www.metacoregames.com{href}")
-            found = True
+
+
 if __name__ == "__main__":
     main()
