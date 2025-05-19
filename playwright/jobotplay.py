1b4aeaa0676140f0eebcdeefe11b34c9c30bb5f9
makekop <72009584+makekop@users.noreply.github.com>
2025-05-19 15:53:23 +0300

    added Rovio as to the script

--- playwright/jobotplay.py
+++ playwright/jobotplay.py
@@ -3,15 +3,13 @@
 from playwright.sync_api import sync_playwright
 
 
-def main():
-
+def get_metacore_jobs():
     with sync_playwright() as p:
         browser = p.chromium.launch(headless=True)
         page = browser.new_page()
         page.goto("https://metacoregames.com/open-positions")
-        page.wait_for_selector(".job-title")
 
-        print("Here are the current job openings:\n ")
+        print("Open Positions at Metacore:\n ")
 
         for link in page.query_selector_all('a[href*="open-positions/"]'):
             text = link.inner_text().strip()
@@ -25,7 +23,35 @@ def main():
                 "url": f"www.metacoregames.com{href}",
             }
 
-            print(f"{job["title"]}, {job['location']}: {job['url']}")
+            print(f"{job["title"]}, {job["location"]}: {job["url"]}")
+        browser.close()
+
+
+def get_rovio_jobs():
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=True)
+        page = browser.new_page()
+        page.goto("https://www.rovio.com/open-positions/")
+
+        print("\nOpen Positions at Rovio:\n ")
+
+        for link in page.query_selector_all('a[href*="open-positions/"]'):
+            href = link.get_attribute("href").strip()
+            if href.endswith("/open-positions/"):
+                continue
+            text = link.inner_text().strip()
+            split = text.split(",")
+            job = {
+                "title": split[0],
+                "url": href,
+            }
+            print(f"{job['title']}: {job["url"]}")
+        browser.close()
+
+
+def main():
+    get_metacore_jobs()
+    get_rovio_jobs()
 
 
 if __name__ == "__main__":
